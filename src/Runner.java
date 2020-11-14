c669dc13415f2379021f01894080ff83b7d2bdc1
ShahnozaSh <shahnoza_3010@mai.ru>
2020-11-14 21:03:17 +0600

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,35 @@
+public class Runner extends Thread{
+    public Runner(String s) {
+        super(s);
+    }
+
+    private int nextRunner;
+    public int index;
+
+    public void setNextRunner(int nextRunner) {
+        this.nextRunner = nextRunner;
+    }
+
+    public int getNextRunner() {
+        return nextRunner;
+    }
+
+    public void run() {
+        if (index == 0) {
+            System.out.println(this.getName() + " берет палочку ");
+            System.out.println(this.getName() + " бежит к Runner " + this.getNextRunner());
+
+        } else {
+            System.out.println(this.getName() + " берет палочку ");
+            if (getNextRunner()+1==5){
+                System.out.println(this.getName() + " бежит к финишу ");
+
+            }
+            System.out.println(this.getName() + " бежит к Runner: " + this.getNextRunner());
+        }
+
+
+    }
+}
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 1; i < 5; i++) {
+            Runner runner=new Runner("Runner: "+i);
+            runner.setNextRunner(i+1);
+            runner.index=0;
+            runner.start();
+            runner.join();
+        }
+
+        for (int i = 5; i > 1; i--) {
+            Runner runner = new Runner("Runner: " +i);
+            runner.setNextRunner(i-1);
+            runner.index=1;
+            runner.start();
+            runner.join();
+        }
+        System.out.println("Runner 1 берет палочку");
+    }
+}
